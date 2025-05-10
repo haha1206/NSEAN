5dc1771d9fc2841939504207d79b022899d7685c
haha1206 <56426332+haha1206@users.noreply.github.com>
2025-05-10 12:44:51 +0800

    Update and rename GRAMC.py to NSEAN.py

--- NSEAN.py
+++ NSEAN.py
@@ -1,45 +1,74 @@
 import numpy as np
-from W_Construct import norm_W,KNN
+from sklearn.metrics.pairwise import euclidean_distances as EuDist2
+from W_Construct import KNN
 from data_loader import load_mat
 import math
 from sklearn import metrics
 import warnings
 warnings.filterwarnings("ignore",category=DeprecationWarning)
-def NMD(X,n_knn,n_cluster,n_iter,lambda_1):
+def NMD(X,k,n_cluster,n_iter):
     n_view = X.size
-    N = X[0].shape[0]
-    W = []
-    for i in range(n_view):
-        W.append(norm_W(KNN(X[i],n_knn)))
-    alpha = np.ones(n_view)/n_view
-    W_new = np.zeros((N,N))
-    for i in range(n_view):
-        W_new += W[i]*alpha[i]
-    val, vec = np.linalg.eigh(W[0])
-    F = vec[:, -n_cluster:]
-    F = WHH(W_new,F,n_cluster)
-    F,alpha = opt(W,F,alpha,n_iter,n_cluster,lambda_1)
-    y_pred = np.argmax(F,axis=1)+1
-    return y_pred,alpha,F
+    num = X[0].shape[0]
 
-def opt(W,  F, alpha, NITER,n_cluster,lambda_1):
+    # normalization
+    for i in range(n_view):
+        for j in range(num):
+            X[i][j, :] = (X[i][j, :] - np.mean(X[i][j, :])) / np.std(X[i][j, :])
 
-    n_view = alpha.size
-    N, _ = W[0].shape
+    # 初始化
+    disX_In = np.zeros((num, num, n_view))
+    index = np.zeros((num, num, n_view))
+    for i in range(n_view):
+        disX_In[:, :, i] = EuDist2(X[i], X[i])
+        index[:, :, i] = np.argsort(disX_In[:, :, i], axis=1)
+    ## init w
+    w = np.ones((n_view, 1)) / n_view
+    #初始化Sv
+    S_mvc = np.zeros((n_view,num, num))
+    S_SUM = np.zeros((num, num))
+    for i in range(n_view):
+        S_mvc[i] = KNN(X[i],k)
+        S_SUM = S_SUM+S_mvc[i]
+    # 初始化F
+    S_int = S_SUM
+    sum_s = np.sum(S_int, axis=1)
+    D = np.zeros_like(S_int)
+    np.fill_diagonal(D, sum_s)
+    L = D - S_int
+    val, vec = np.linalg.eigh(L)
+    F = vec[:, :n_cluster]
+
+    y_pred,G= opt(S_mvc,F,disX_In,index,w,k,n_iter,n_cluster)
+    return y_pred,G
+def opt(S_mvc,  F,disX_In,index, w, k,NITER,n_cluster):
+
+    n_view = w.size
+    N, _ = S_mvc[0].shape
     FF = F @ F.T
+    eps = 2.2204e-16
     for Iter in range(NITER):
         W_new = np.zeros((N, N))
-        P_v = np.zeros(n_view)
-        #alpha
+        #update Sv
+        for v in range(n_view):
+            for i in range(N):
+                id = index[:,:,v][i, 1:(k + 2)]
+                id = list(map(int, id))
+                di = disX_In[:,:,v][i, id]
+                numerator = di[k] - di + 2 * w[v]*  FF[i, id]-2 * w[v]*  FF[i, id[k]]
+                denominator1 = k * di[k] - sum(di[:k-1])
+                denominator2 = 2 * w[v]* sum(FF[i, id[:k]])-2 * k * w[v] * FF[i, id[k]]
+                S_mvc[v][i, id] = numerator / (denominator1 + denominator2 + eps)
+                S_mvc[v][i, id][S_mvc[v][i, id]<0]=0
+        #update w
         for i in range(n_view):
-            P_v[i] = (1/(2*lambda_1))*np.square(np.linalg.norm(W[i]-FF))
-        alpha = EProjSimplex_new(-P_v)
+            w[i] = 0.5/np.linalg.norm(S_mvc[i]-FF)
         for i in range(n_view):
-            W_new += W[i] * alpha[i]
-        # update F
-        F = WHH(W_new ,F,n_cluster )
+            S_mvc[i] = (S_mvc[i] + S_mvc[i].T) / 2
+            W_new += S_mvc[i] * w[i]
+        F = WHH(W_new ,F,n_cluster)
+        y_pred = np.argmax(F, axis=1) + 1
+    return y_pred,F
 
-    return F,alpha
 
 def EProjSimplex_new(v,k=1):
     ft = 1
@@ -66,10 +95,8 @@ def EProjSimplex_new(v,k=1):
     else:
         x = v_0
     return x
-
-
-def WHH(W, F, c,mu=3,rou=1.5):
-    N_inter =10
+def WHH(W, F, c,mu = 0.1,rou= 1.005):
+    N_inter = 10
     threshold = 1e-10
     val = 0
     cnt = 0
@@ -83,9 +110,8 @@ def WHH(W, F, c,mu=3,rou=1.5):
         A = np.concatenate((-1*np.identity(c),np.zeros((n-c,c))),axis=0)
         F = U@A@VT
         #update G
-        H = F+(1/mu)*lambda_n+(1/mu)*W@F
-        G[H > 0] = H[H > 0]
-        G[H <= 0] = 0
+        G = F+(1/mu)*lambda_n+(1/mu)*W@F
+        G[G < 0] = 0
         #update lambda_n
         lambda_n = lambda_n + mu*(F-G)
         #update mu
@@ -160,11 +186,51 @@ def purity_score(y_true, y_pred):
 
     return metrics.accuracy_score(y_true, y_voted_labels)
 
+def F_score(labels_true, labels_pred):
+    n = len(labels_true)
+    tp, fp, tn, fn = 0, 0, 0, 0
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if labels_true[i] == labels_true[j] and labels_pred[i] == labels_pred[j]:
+                tp += 1
+            elif labels_true[i] != labels_true[j] and labels_pred[i] == labels_pred[j]:
+                fp += 1
+            elif labels_true[i] == labels_true[j] and labels_pred[i] != labels_pred[j]:
+                fn += 1
+            else:
+                tn += 1
+
+    Pre = tp / (tp + fp)
+    Rec = tp / (tp + fn)
+
+    F_score = 2*Pre*Rec / (Pre+Rec)
+
+    return F_score
+
+def NE(y_pred):
+    n = len(y_pred)
+    ar, num = np.unique(y_pred, return_counts=True)
+    c = len(ar)
+    ne = 0
+    for i in range(c):
+        ne = ne + (num[i]/n)*np.log(num[i]/n)
+    return (-1/np.log(c))*ne
 
 if __name__=="__main__":
-    X,GT = load_mat('data/HW2.mat')
+
+    import time
+    X,GT = load_mat('data/100leaves.mat')
+    n_cluster = len(np.unique(GT))
+    N = X[0].shape[0]
     GT = GT.reshape(np.max(GT.shape), )
-    y_pred,alpha,F = NMD(X,15,10,20,100)
+    c = n_cluster
+    GT = GT.reshape(np.max(GT.shape), )
+    t1 = time.time()
+    n_cluster = len(np.unique(GT))
+    y_pred,F= NMD(X,20,n_cluster,20)
+    t2 = time.time()
+    print(t2-t1)
     ACC = acc(GT, y_pred)
     NMI = metrics.normalized_mutual_info_score(GT, y_pred)
     Purity = purity_score(GT, y_pred)
